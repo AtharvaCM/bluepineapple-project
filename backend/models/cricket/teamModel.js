a0522146af4b33e75276c8cdf4b6b0bf66de3890
AtharvaCM <60681622+AtharvaCM@users.noreply.github.com>
2022-04-20 16:52:05 +0530

    Development (#1)
    * Node Express boilerplate
    
    * updates
    
    * Mongo connection and .env added
    
    * folder structure
    
    * Teams List&Detail(Cricket) working endpoints added

---
+++ backend/models/cricket/teamModel.js
@@ -0,0 +1,26 @@
+/*
+Created: 20th, April, 2022
+Updated: 20th, April, 2022
+Author: AtharvaCM
+Synopsis: MongoDB Model for Cricket Team collection
+Exports: Team model
+*/
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const cricketTeamTable = new Schema(
+  {
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    flag: { type: String, required: true },
+    continent: { type: String, required: true },
+    stats: { type: Object },
+    current_roster: { type: Array },
+  },
+  { timeStamps: true }
+);
+
+const Team = mongoose.model("Team", cricketTeamTable);
+
+module.exports = Team;
