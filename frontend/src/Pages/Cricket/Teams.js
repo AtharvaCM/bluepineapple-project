8278f3014429bbea10d637560ba446f6bae07567
akshayvishwakarma00008 <akshayvishwakarma00008@gmail.com>
2022-04-29 12:28:39 +0530

    .

--- frontend/src/Pages/Cricket/Teams.js
+++ frontend/src/Pages/Cricket/Teams.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-import { CardGroup } from 'react-bootstrap'
+//import { CardGroup } from 'react-bootstrap'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Container } from 'react-bootstrap'
