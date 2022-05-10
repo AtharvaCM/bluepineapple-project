4ade6fc3130f9eb135b057b535d26c3229931eff
AtharvaCM <necrons999@gmail.com>
2022-05-10 13:55:54 +0530

    API changes

--- frontend/src/Api/CurrentMatchesApi.js
+++ frontend/src/Api/CurrentMatchesApi.js
@@ -1,4 +1,4 @@
-// const matchInfo = async () => {
+// const CurrentMatchesApi = async () => {
 //   //const url = "https://api.cricapi.com/v1/matches?apikey=50a551b3-0883-465a-a2b1-e10241452e7f";
 //   const url =
 //     "https://api.cricapi.com/v1/currentMatches?apikey=50a551b3-0883-465a-a2b1-e10241452e7f";
@@ -7,7 +7,7 @@
 //     .catch((error) => console.log(error));
 // };
 
-const MatchInfo = async () => {
+const CurrentMatchesAPI = async () => {
   try {
     const url = "http://127.0.0.1:5000/api/v1/cricket/currentMatches";
     const response = await fetch(url, {
@@ -23,4 +23,4 @@ const MatchInfo = async () => {
     console.log(`{ error: ${err.message || err.toString()} }`);
   }
 };
-export default MatchInfo;
+export default CurrentMatchesAPI;
