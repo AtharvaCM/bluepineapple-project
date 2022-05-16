02648e3638ddba838710e7c3d1a123503c4b23c8
mokshadamalpani <malpanimokshada@gmail.com>
2022-05-16 18:17:35 +0530

    error resolved

--- frontend/src/Pages/Football/Gallery.js
+++ frontend/src/Pages/Football/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../../Resources/Styles/Gallery.css'
 import FootballSubNavBar from './FootballSubNavBar'
 import gallery from '../../Api/footballApi/gallery'
@@ -6,16 +6,13 @@ import gallery from '../../Api/footballApi/gallery'
 function Gallery() {
   console.log('hello from football gallery')
   console.log(gallery);
-  const [Details,setDetails]=useState(gallery)
-  var index=0
   return (
     <>
-      
       <FootballSubNavBar></FootballSubNavBar>
       <h1>The Football Gallery</h1>
       <p className='galleryPara'>Some memories to cherish...</p>
       <div className="masonry">
-      {Details.map(Team => {
+      {gallery.map(Team => {
           return (
             <div className="item" key={Team.id} style={{color:'white'}}>   
              <h5>{Team.Title}</h5>
