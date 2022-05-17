649af57ff54e631766883eb492156c1194c23015
mokshadamalpani <malpanimokshada@gmail.com>
2022-05-17 13:30:33 +0530

    mm

--- frontend/src/Pages/Football/Gallery.js
+++ frontend/src/Pages/Football/Gallery.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import '../../Resources/Styles/Gallery.css'
 import FootballSubNavBar from './FootballSubNavBar'
-import gallery from '../../Api/footballApi/gallery'
+//import gallery from '../../Api/footballApi/gallery'
 
 function Gallery() {
   console.log('hello from football gallery')
-  console.log(gallery);
+  //console.log(gallery);
   return (
     <>
       <FootballSubNavBar></FootballSubNavBar>
       <h1>The Football Gallery</h1>
       <p className='galleryPara'>Some memories to cherish...</p>
       <div className="masonry">
-      {gallery.map(Team => {
+      {/* {gallery.map(Team => {
           return (
             <div className="item" key={Team.id} style={{color:'white'}}>   
              <h5>{Team.Title}</h5>
@@ -20,7 +20,7 @@ function Gallery() {
              </div>
           )
         })
-      }
+      } */}
       </div>
       
     </>
