ed9070c47dc838d80769187682a8e22524e31b74
mokshadamalpani <malpanimokshada@gmail.com>
2022-05-17 12:12:57 +0530

    xx

---
+++ frontend/src/Api/footballApi/FootballGalleryAPI.js
@@ -0,0 +1,17 @@
+const FootballGalleryAPI = async () => {
+    try {
+      const url = "http://127.0.0.1:5000/api/v1/football/teamsList";
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {},
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const res = await response.json();
+      return res;
+    } catch (err) {
+      console.log(`{ error: ${err.message || err.toString()} }`);
+    }
+  };
+  export default FootballGalleryAPI;
