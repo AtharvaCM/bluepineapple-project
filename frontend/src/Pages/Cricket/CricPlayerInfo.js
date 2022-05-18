1d76634424d275dc11bf7cf539ebfc16bdb9b96b
mokshadamalpani <malpanimokshada@gmail.com>
2022-05-18 22:49:02 +0530

    career summary done

--- frontend/src/Pages/Cricket/CricPlayerInfo.js
+++ frontend/src/Pages/Cricket/CricPlayerInfo.js
@@ -79,41 +79,41 @@ function CricPlayerInfo(prop) {
                             <th>HS</th>
                             <th>Avg</th>
                             <th>SR</th>
-                            <th>100</th>
-                            <th>50</th>
+                            {/* <th>100</th>
+                            <th>50</th> */}
                         </tr>
                         <tr>
                             <td>Test</td>
-                            <td>{PlayerDetails[0].batting.test.maches}</td>
-                            <td>{PlayerDetails[0].batting.test.innings}</td>
-                            <td>{PlayerDetails[0].batting.test.runs}</td>
-                            <td>{PlayerDetails[0].batting.test.high_score}</td>
-                            <td>{PlayerDetails[0].batting.test.batting_avg}</td>
-                            <td>{PlayerDetails[0].batting.test.sr}</td>
-                            <td>2</td>
-                            <td>55</td>
+                            <td>{PlayerDetails[0].batting.test.maches  === '' ? '-' :PlayerDetails[0].batting.test.maches}</td>
+                            <td>{PlayerDetails[0].batting.test.innings  === '' ? '-' :PlayerDetails[0].batting.test.innings}</td>
+                            <td>{PlayerDetails[0].batting.test.runs  === '' ? '-' :PlayerDetails[0].batting.test.runs}</td>
+                            <td>{PlayerDetails[0].batting.test.high_score  === '' ? '-' :PlayerDetails[0].batting.test.high_score }</td>
+                            <td>{PlayerDetails[0].batting.test.batting_avg  === '' ? '-' :PlayerDetails[0].batting.test.batting_avg }</td>
+                            <td>{PlayerDetails[0].batting.test.sr  === '' ? '-' :PlayerDetails[0].batting.test.sr}</td>
+                            {/* <td>{PlayerDetails[0].batting.test.hundreds}</td>
+                            <td>{PlayerDetails[0].batting.test.fifties}</td> */}
                         </tr>
                         <tr>
                             <td>ODI</td>
-                            <td>{PlayerDetails[0].batting.odi.maches}</td>
-                            <td>{PlayerDetails[0].batting.odi.innings}</td>
-                            <td>{PlayerDetails[0].batting.odi.runs}</td>
-                            <td>{PlayerDetails[0].batting.odi.high_score}</td>
-                            <td>{PlayerDetails[0].batting.odi.batting_avg}</td>
-                            <td>{PlayerDetails[0].batting.odi.sr}</td>
-                            <td>2</td>
-                            <td>55</td>
+                            <td>{PlayerDetails[0].batting.odi.maches  === '' ? '-' :PlayerDetails[0].batting.odi.maches}</td>
+                            <td>{PlayerDetails[0].batting.odi.innings  === '' ? '-' :PlayerDetails[0].batting.odi.innings}</td>
+                            <td>{PlayerDetails[0].batting.odi.runs  === '' ? '-' :PlayerDetails[0].batting.odi.runs}</td>
+                            <td>{PlayerDetails[0].batting.odi.high_score  === '' ? '-' :PlayerDetails[0].batting.odi.high_score }</td>
+                            <td>{PlayerDetails[0].batting.odi.batting_avg  === '' ? '-' :PlayerDetails[0].batting.odi.batting_avg}</td>
+                            <td>{PlayerDetails[0].batting.odi.sr  === '' ? '-' :PlayerDetails[0].batting.odi.sr}</td>
+                            {/* <td>2</td>
+                            <td>55</td> */}
                         </tr>
                         <tr>
                             <td>T20l</td>
-                            <td>{PlayerDetails[0].batting.t20.maches}</td>
-                            <td>{PlayerDetails[0].batting.t20.innings}</td>
-                            <td>{PlayerDetails[0].batting.t20.runs}</td>
-                            <td>{PlayerDetails[0].batting.t20.high_score}</td>
-                            <td>{PlayerDetails[0].batting.t20.batting_avg}</td>
-                            <td>{PlayerDetails[0].batting.t20.sr}</td>
-                            <td>2</td>
-                            <td>55</td>
+                            <td>{PlayerDetails[0].batting.t20.maches  === '' ? '-' :PlayerDetails[0].batting.t20.maches}</td>
+                            <td>{PlayerDetails[0].batting.t20.innings  === '' ? '-' :PlayerDetails[0].batting.t20.innings}</td>
+                            <td>{PlayerDetails[0].batting.t20.runs  === '' ? '-' :PlayerDetails[0].batting.t20.runs}</td>
+                            <td>{PlayerDetails[0].batting.t20.high_score  === '' ? '-' :PlayerDetails[0].batting.t20.high_score }</td>
+                            <td>{PlayerDetails[0].batting.t20.batting_avg  === '' ? '-' :PlayerDetails[0].batting.t20.batting_avg}</td>
+                            <td>{PlayerDetails[0].batting.t20.sr  === '' ? '-' :PlayerDetails[0].batting.t20.sr}</td>
+                            {/* <td>2</td>
+                            <td>55</td> */}
                         </tr>
                     </table>
                 </Card.Body>
@@ -136,36 +136,36 @@ function CricPlayerInfo(prop) {
                         </tr>
                         <tr>
                             <td>Test</td>
-                            <td>{PlayerDetails[0].bowling.test.maches}</td>
-                            <td>{PlayerDetails[0].bowling.test.innings}</td>
-                            <td>{PlayerDetails[0].bowling.test.wickets}</td>
-                            <td>{PlayerDetails[0].bowling.test.Economy}</td>
-                            <td>{PlayerDetails[0].bowling.test.bowling_avg}</td>
-                            <td>{PlayerDetails[0].bowling.test.sr}</td>
-                            <td>4</td>
-                            <td>55</td>
+                            <td>{PlayerDetails[0].bowling.test.matches  === '' ? '-' :PlayerDetails[0].bowling.test.matches}</td>
+                            <td>{PlayerDetails[0].bowling.test.innings  === '' ? '-' :PlayerDetails[0].bowling.test.innings}</td>
+                            <td>{PlayerDetails[0].bowling.test.wickets  === '' ? '-' :PlayerDetails[0].bowling.test.wickets}</td>
+                            <td>{PlayerDetails[0].bowling.test.Economy  === '' ? '-' :PlayerDetails[0].bowling.test.Economy }</td>
+                            <td>{PlayerDetails[0].bowling.test.bowling_avg  === '' ? '-' :PlayerDetails[0].bowling.test.bowling_avg }</td>
+                            <td>{PlayerDetails[0].bowling.test.sr === '' ? '-' :PlayerDetails[0].bowling.test.sr}</td>
+                            <td>{PlayerDetails[0].bowling.test.five_w === '' ? '-' :PlayerDetails[0].bowling.test.five_w}</td>
+                            <td>{PlayerDetails[0].bowling.test.ten_w  === '' ? '-' :PlayerDetails[0].bowling.test.ten_w }</td>
                         </tr>
                         <tr>
                             <td>ODI</td>
-                            <td>{PlayerDetails[0].bowling.odi.maches}</td>
-                            <td>{PlayerDetails[0].bowling.odi.innings}</td>
-                            <td>{PlayerDetails[0].bowling.odi.wickets}</td>
-                            <td>{PlayerDetails[0].bowling.odi.Economy}</td>
-                            <td>{PlayerDetails[0].bowling.odi.bowling_avg}</td>
-                            <td>{PlayerDetails[0].bowling.odi.sr}</td>
-                            <td>2</td>
-                            <td>55</td>
+                            <td>{PlayerDetails[0].bowling.odi.matches === '' ? '-' : PlayerDetails[0].bowling.odi.matches }</td>
+                            <td>{PlayerDetails[0].bowling.odi.innings  === '' ? '-' : PlayerDetails[0].bowling.odi.innings}</td>
+                            <td>{PlayerDetails[0].bowling.odi.wickets  === '' ? '-' :PlayerDetails[0].bowling.odi.wickets}</td>
+                            <td>{PlayerDetails[0].bowling.odi.Economy  === '' ? '-' :PlayerDetails[0].bowling.odi.Economy}</td>
+                            <td>{PlayerDetails[0].bowling.odi.bowling_avg  === '' ? '-' :PlayerDetails[0].bowling.odi.bowling_avg}</td>
+                            <td>{PlayerDetails[0].bowling.odi.sr  === '' ? '-' :PlayerDetails[0].bowling.odi.sr}</td>
+                            <td>{PlayerDetails[0].bowling.odi.five_w  === '' ? '-' :PlayerDetails[0].bowling.odi.five_w}</td>
+                            <td>{PlayerDetails[0].bowling.odi.ten_w  === '' ? '-' :PlayerDetails[0].bowling.odi.ten_w}</td>
                         </tr>
                         <tr>
                             <td>T20l</td>
-                            <td>{PlayerDetails[0].bowling.t20.maches}</td>
-                            <td>{PlayerDetails[0].bowling.t20.innings}</td>
-                            <td>{PlayerDetails[0].bowling.t20.wickets}</td>
-                            <td>{PlayerDetails[0].bowling.t20.Economy}</td>
-                            <td>{PlayerDetails[0].bowling.t20.batting_avg}</td>
-                            <td>{PlayerDetails[0].bowling.t20.sr}</td>
-                            <td>2</td>
-                            <td>55</td>
+                            <td>{PlayerDetails[0].bowling.t20.matches  === '' ? '-' :PlayerDetails[0].bowling.t20.matches}</td>
+                            <td>{PlayerDetails[0].bowling.t20.innings  === '' ? '-' :PlayerDetails[0].bowling.t20.innings}</td>
+                            <td>{PlayerDetails[0].bowling.t20.wickets  === '' ? '-' :PlayerDetails[0].bowling.t20.wickets}</td>
+                            <td>{PlayerDetails[0].bowling.t20.Economy  === '' ? '-' :PlayerDetails[0].bowling.t20.Economy}</td>
+                            <td>{PlayerDetails[0].bowling.t20.bowling_avg  === '' ? '-' :PlayerDetails[0].bowling.t20.bowling_avg}</td>
+                            <td>{PlayerDetails[0].bowling.odi.sr  === '' ? '-' :PlayerDetails[0].bowling.odi.sr}</td>
+                            <td>{PlayerDetails[0].bowling.t20.five_w  === '' ? '-' :PlayerDetails[0].bowling.t20.five_w}</td>
+                            <td>{PlayerDetails[0].bowling.t20.ten_w  === '' ? '-' :PlayerDetails[0].bowling.t20.ten_w }</td>
                         </tr>
                     </table>
                 </Card.Body>
