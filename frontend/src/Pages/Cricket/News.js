c7f7c8c30c7f46e673e4a07797bf73dd3f9662b2
mokshadamalpani <malpanimokshada@gmail.com>
2022-05-19 19:55:53 +0530

    cc

--- frontend/src/Pages/Cricket/News.js
+++ frontend/src/Pages/Cricket/News.js
@@ -46,37 +46,6 @@ function News() {
 
         <Container sx={{ mt: 2 }}>
 
-<<<<<<< HEAD
-          <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-              {news.map((data, index) => (
-                <Grid item xs={4} key={index}>
-                  <Badge pill bg="danger">{data.source.name}</Badge>
-                  <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={data.urlToImage}
-                      alt="img"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {data.title}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {data.description}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small">Share</Button>
-                      <Button size="small" href={data.url} target="_blank">Learn More</Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
-=======
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
             {news.map((data, index) => (
@@ -105,7 +74,6 @@ function News() {
             ))}
           </Grid>
         </Box>
->>>>>>> 61584b81c41418326b0b9091e161125024821d73
         </Container>
 
       </>
